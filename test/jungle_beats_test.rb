787ea8e93605c9a4ec7bfc2a0f63c88e32c4c4a0
Ilana Corson <ilana@turing.io>
2017-05-17 12:16:18 -0600

    [Finishes #145558273] finish

--- test/jungle_beats_test.rb
+++ test/jungle_beats_test.rb
@@ -5,7 +5,7 @@ require './lib/jungle_beats'
 class JungleBeatsTest < Minitest::Test
 
   def test_test_test
-    assert_equal nil
+    assert_ealkdjf;lakdjf
   end
 
   def test_new_beat_asserts_empty_linked_list
